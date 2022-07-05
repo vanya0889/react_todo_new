990a62af465fc3b7137e5a57894c607f9fb1ce1c
vanya0889 <bondarenko.fortech@gmail.com>
2022-07-05 16:30:31 +0300

    add functions

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,28 @@
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import ToDoForm from "./todo-form/ToDoForm"
 import ToDo from "./tasks/ToDo"
 import style from "./App.module.css"
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const [viewOptions, setViewOptions] = useState("all")
+
 
   const [todos, setTodos] = useState([]);
+
+  const filteredTasks = useMemo(() => {
+	switch (viewOptions) {
+	  case "all":
+		return todos;
+	  case "checked":
+		return todos.filter((todo) => todo.complete === true);
+	  case "unchecked":
+		return todos.filter((todo) => todo.complete === false);
+	  default:
+		return
+	}
+  }, [viewOptions, todos])
 
   const addTask = (userInput) => {
 	if (userInput) {
@@ -14,16 +31,30 @@ function App() {
 		task: userInput,
 		complete: false
 	  }
-setTodos([...todos,newItem])
+	  setTodos([...todos, newItem])
 	}
   }
+  const checkAll = () => {
+    setTodos(todos.map((element) => {
+	  return {
+	    ...element,
+		complete: true
+	  }
+	}))
+  }
+
+
+
+  const deleteChecked = () => {
+	setTodos(todos.filter(element => element.complete === false))
+  }
   const removeTask = (id) => {
-setTodos([...todos.filter((todo) => todo.id !== id)])
+	setTodos([...todos.filter((todo) => todo.id !== id)])
   }
   const handleTouggle = (id) => {
-setTodos([...todos.map((todo) =>
-todo.id === id ? {...todo, complete: !todo.complete} : {...todo}
-)])
+	setTodos([...todos.map((todo) =>
+	  todo.id === id ? {...todo, complete: !todo.complete} : {...todo}
+	)])
   }
 
   return (
@@ -31,19 +62,42 @@ todo.id === id ? {...todo, complete: !todo.complete} : {...todo}
 
 	  <br/>
 	  <h1>Your todos list</h1>
-	  <div className={style.container} >
+	  <div className={style.container}>
 		<ToDoForm addTask={addTask}/>
-		{todos.map((todo) => {
+		{filteredTasks.map((todo) => {
 		  return (
 			<ToDo
 			  todo={todo}
 			  key={todo.id}
+			  isCheck={todo.complete}
 			  text={todo.task}
 			  toggleTask={handleTouggle}
 			  removeTask={removeTask}
 			/>
 		  )
 		})}
+
+		<div className={style.filter}>
+		  {
+			todos.length > 0 &&
+
+			<ButtonGroup  aria-label="Basic example">
+			  <span onClick={() => checkAll()} className={style.sec}>{todos.filter(element => element.complete === false).length}tasks left</span>
+			  <Button className={style.btn} variant="secondary" onClick={() => setViewOptions("all")}>All</Button>
+			  <Button variant="secondary" onClick={() => setViewOptions("unchecked")}>ToDo</Button>
+			  <Button variant="secondary" onClick={() => setViewOptions("checked")}>Completed</Button>
+			</ButtonGroup>
+
+		  }
+		  {
+			todos.some(element => element.complete === true) && <ButtonGroup aria-label="Basic example">
+			  <Button variant="secondary" onClick={() => deleteChecked()}>Clear completed</Button>
+			</ButtonGroup>
+		  }
+
+		</div>
+
+
 	  </div>
 	</div>
   );
--- todo/src/tasks/ToDo.js
+++ todo/src/tasks/ToDo.js
@@ -1,31 +1,20 @@
 import style from "./ToDo.module.css"
 import { FaTrashAlt } from 'react-icons/fa';
-import ButtonGroup from "react-bootstrap/ButtonGroup";
-import Button from "react-bootstrap/Button";
-import {useState} from "react";
 
 
 
-function ToDo({todo, toggleTask, removeTask, text}) {
 
-  const [filtered, setFiltered] = useState(todo)
+function ToDo({todo, toggleTask, removeTask, text, isCheck}) {
 
 
-  const todofilter = (complete) => {
-	if (complete==='all') {
-	  setFiltered(todo)
-	} else {
-	  let newTodo = [...todo].filter(item => item.complete === complete)
-	  setFiltered(newTodo)
-	}
-  }
 
   return (
     <div>
 	  <div key={todo.id} className={style.item}>
 		<div
-		  className={todo.complete ? style.item & style.strike : style.item }
-		  onClick={() => toggleTask(todo.id)}>
+		  className={todo.complete ? style.strike : style.item }
+		  >
+		  <input className={style.check} checked={isCheck} onClick={() => toggleTask(todo.id)} type="checkbox"/>
 		  {todo.task}
 		</div>
 
@@ -33,11 +22,6 @@ function ToDo({todo, toggleTask, removeTask, text}) {
 
 	  </div>
 	  <hr/>
-	  <ButtonGroup aria-label="Basic example">
-		<Button variant="secondary" onClick={()=>todofilter('all')}>All</Button>
-		<Button variant="secondary" onClick={()=>todofilter(true)}>ToDo</Button>
-		<Button variant="secondary" onClick={()=>todofilter(false)}>Completed</Button>
-	  </ButtonGroup>
 	</div>
 
 
